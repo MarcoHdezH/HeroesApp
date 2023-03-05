a8ac1daf459b9c39ea6c6b59d9d47c32ef4a5aa2
Marco Hernandez <marco7245@hotmail.com>
2023-03-04 18:31:31 -0600

    Mas Informacion

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
@@ -10,6 +11,22 @@ export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance
                     <div className='col-4'>
                         <img src={heroImageUrl} className='card-img' alt={superhero}/>
                     </div>
+
+                    <div className='col-8'>
+                        <div className='card-body'>
+                            <h5 className='card-title'>{superhero}</h5>
+                            <p className='card-text'>{alter_ego}</p>
+                            {
+                                (alter_ego !== characters ) && (<p>{characters}</p>)
+                            }
+                            <p className='card-text'> <small className='text-muted'>{first_appearance}</small> </p>
+
+                            <Link to={`/Hero`}>Más...</Link>
+
+                        </div>
+                    </div>
+
+                    
                 </div>
             </div>
 
