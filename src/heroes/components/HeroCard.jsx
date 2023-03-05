c4ff08684fcb8712db48ad1e2e5038b38945d3c5
Marco Hernandez <marco7245@hotmail.com>
2023-03-04 18:24:52 -0600

    Cards Añadidas

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 export const HeroCard = ({ id, superhero, publisher, alter_ego, first_appearance, characters }) => {
 
-    const heroImageUrl = null;
+    const heroImageUrl = `/assets/heroes/${id}.jpg`;
     return (
         <div className='col'>
             <div className='card'>
                 <div className='row no-gutters'>
                     <div className='col-4'>
-                        <img src='../../' className='card-img' alt={superhero}/>
+                        <img src={heroImageUrl} className='card-img' alt={superhero}/>
                     </div>
                 </div>
             </div>
