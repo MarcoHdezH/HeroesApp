0d8fc1d75b865be69590e939c0191fe136372f09
Marco Hernandez <marco7245@hotmail.com>
2023-03-05 21:58:12 -0600

    Proteccion de Rutas iniciada

--- src/UI/components/Navbar.jsx
+++ src/UI/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context/AuthContext';
 
 
 export const Navbar = () => {
+
+    const { user } = useContext(AuthContext);
     
     const navigate = useNavigate();
     const onLogout = () =>{
@@ -27,7 +31,7 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-md-end">
                 <ul className="navbar-nav ml-auto">
-                <span className='nav-item nav-link text-primary'>Marco Hernandez</span>
+                <span className='nav-item nav-link text-primary'>{user?.name}</span>
                 <button className='nav-item nav-link btn' onClick={ onLogout }>LogOut</button>
                 </ul>
             </div>
